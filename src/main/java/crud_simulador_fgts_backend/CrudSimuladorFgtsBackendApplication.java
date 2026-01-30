d346853238d320bbb26687480231501102808a0f
Claudir Fantuci <claudirfantucijunior@gmail.com>
2026-01-29 22:53:36 -0300

    first commit

---
+++ src/main/java/crud_simulador_fgts_backend/CrudSimuladorFgtsBackendApplication.java
@@ -0,0 +1,13 @@
+package crud_simulador_fgts_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSimuladorFgtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSimuladorFgtsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crud_simulador_fgts_backend/CrudSimuladorFgtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package crud_simulador_fgts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSimuladorFgtsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
